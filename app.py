2ba6a27bdbb15734353f53d9ca99db2a8ab68948
Nagamohan Burugupalli <nagamohanburugupalli@gmail.com>
2022-03-26 12:54:24 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,101 @@
+import os
+from flask import Flask,render_template,request,redirect, url_for, flash
+import gunicorn
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app=Flask(__name__)
+app.secret_key = "Secret Key"
+
+basedir=os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///'+os.path.join(basedir,'data.sqllite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
+db=SQLAlchemy(app)  
+Migrate(app,db)
+
+class Data(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    phone = db.Column(db.String(100))
+    
+    def __init__(self,name,email,phone): #constructor is called when object is called
+        self.name=name
+        self.email=email
+        self.phone=phone
+        
+        # Route for handling the login page logic
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('Index'))
+    return render_template('login.html', error=error)
+
+
+
+
+#query on all our Student data
+@app.route('/home')
+def Index():
+    all_data = Data.query.all()
+
+    return render_template("index.html", students = all_data)
+
+
+
+#this route is for inserting data to mysql database via html forms
+@app.route('/insert', methods = ['POST'])
+def insert():
+
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+
+
+        my_data = Data(name, email, phone)
+        db.session.add(my_data)
+        db.session.commit()
+
+        flash("Employee Inserted Successfully")
+
+        return redirect(url_for('Index'))
+
+
+#this is our update route where we are going to update our employee
+@app.route('/update', methods = ['GET', 'POST'])
+def update():
+
+    if request.method == 'POST':
+        my_data = Data.query.get(request.form.get('id'))
+        my_data.name = request.form['name']
+        my_data.email = request.form['email']
+        my_data.phone = request.form['phone']
+
+        db.session.commit()
+        flash("Employee Updated Successfully")
+
+        return redirect(url_for('Index'))
+
+
+
+
+#This route is for deleting our employee
+@app.route('/delete/<id>/', methods = ['GET', 'POST'])
+def delete(id):
+    my_data = Data.query.get(id)
+    db.session.delete(my_data)
+    db.session.commit()
+    flash("Employee Deleted Successfully")
+
+    return redirect(url_for('Index'))
+
+
+if __name__=="__main__":
+    app.run(debug=True)
+    
